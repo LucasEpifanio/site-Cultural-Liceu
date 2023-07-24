0f55ba1bb22df0f790c27540463d4c9b24d31d81
LucasEpifanio <Lucasepifaniodev@gmail.com>
2023-07-24 13:28:48 +0100

    site

---
+++ menu.js
@@ -0,0 +1,5 @@
+/*menu-scroll*/
+window.addEventListener("scroll", function(){
+    let nav = document.querySelector('#nav')
+    nav.classList.toggle('rolagem',window.scrollY > 70)
+})
---
+++ script.js
@@ -0,0 +1,6 @@
+
+window.sr = ScrollReveal({reset: true });
+
+sr.reveal('.icon', {duration: 2000});
+
+sr.reveal('.site-logo', {duration: 2000});
